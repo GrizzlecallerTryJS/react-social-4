82704f03d58c046972aeb9f3c82a6db6776c14fb
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-21 17:03:15 +0300

    L11: add components + css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Header from './Header/Header'
+import Sidebar from './Sidebar/Sidebar'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <div className='header'><Header /></div>
+      <div className='sidebar'><Sidebar /></div>
+      <div className='main'><Main /></div>
+      <div className='footer'><Footer /></div>
     </div>
   );
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        Header
+      </div>
+      <div>
+        <img src="https://e7.pngegg.com/pngimages/398/8/png-clipart-butterfly-logo-logo-design-fashion-butterfly-flag-fashion-girl-thumbnail.png" alt="logo" />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
