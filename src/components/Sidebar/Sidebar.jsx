07cc421d4d83448783c712c9164d61b2808761d5
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-22 00:29:22 +0300

    L14: remade css to module.css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
   return (
-    <div>
-      <div>smth1</div>
-      <div>smth2</div>
-      <div>smth3</div>
-      <div>smth4</div>
-      <div>smth5</div>
+    <div className={styles.sidebar}>
+      <div className={styles.item}>
+        <div>smth1</div>
+        <div>smth2</div>
+        <div>smth3</div>
+        <div>smth4</div>
+        <div>smth5</div>
+      </div>
     </div>
   )
 }
