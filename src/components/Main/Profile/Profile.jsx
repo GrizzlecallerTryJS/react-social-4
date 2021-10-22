cba6069440a883105fb705b19efa6cb797c69f57
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-22 15:37:15 +0300

    L15: change structure of components

--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import styles from './Profile.module.css';
-import Post from './Post/Post';
+import React from "react";
+import styles from "./Profile.module.css";
+import Post from "./Post_modules/Post/Post";
+import NewPost from "./Post_modules/NewPost/NewPost";
 
 const Main = () => {
   return (
     <div className={styles.main}>
-      <div>
-        Main
-      </div>
+      <div>Main</div>
       <div className={styles.about}>
         avatar + desc
         <div>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1vgdYWHDUkyYYYxV4RV78Q4AHDtagK2GRQ&usqp=CAU" alt="default avatar image" />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1vgdYWHDUkyYYYxV4RV78Q4AHDtagK2GRQ&usqp=CAU"
+            alt="default avatar image"
+          />
         </div>
       </div>
+      <NewPost />
+      <Post />
       <Post />
     </div>
-  )
-}
+  );
+};
 
 export default Main;
