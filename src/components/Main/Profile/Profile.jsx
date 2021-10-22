95721720607cd245240ceeaefc40db52a132e5b3
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-22 13:56:22 +0300

    L15: component separation

--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Profile.module.css';
+import Post from './Post/Post';
 
 const Main = () => {
   return (
@@ -13,18 +14,7 @@ const Main = () => {
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1vgdYWHDUkyYYYxV4RV78Q4AHDtagK2GRQ&usqp=CAU" alt="default avatar image" />
         </div>
       </div>
-      <div className={styles.posts}>
-        my posts
-        <div>
-          new post
-        </div>
-        <div>
-          post1
-        </div>
-        <div>
-          post2
-        </div>
-      </div>
+      <Post />
     </div>
   )
 }
