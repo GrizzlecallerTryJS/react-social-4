0ca6ef5c5963a63c7d19ce72da0c0ef9bb3f95ce
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-25 18:41:35 +0300

    L30: add button and action

--- src/components/Main/Profile/Posts/PostComponents/NewPost/NewPost.jsx
+++ src/components/Main/Profile/Posts/PostComponents/NewPost/NewPost.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const NewPost = () => {
+  let textAreaRef = React.createRef();
+
+  const addButtonAction = () => {
+    let text = textAreaRef.current.value;
+    alert(text);
+  };
+
   return (
     <div>
       <p>New post</p>
-      <textarea name="newPost"></textarea>
+      <div>
+        <textarea ref={textAreaRef} name="newPost"></textarea>
+        <button onClick={addButtonAction}>addPost</button>
+      </div>
     </div>
   );
 };
