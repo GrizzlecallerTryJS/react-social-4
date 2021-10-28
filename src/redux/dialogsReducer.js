734d56ee31e37c29047500d8eb3c099eb045c5a3
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-10-28 21:38:40 +0300

    L42: add redux

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -6,7 +6,66 @@ import {
 const SEND_MESSAGE = "SEND_MESSAGE";
 const CURRENT_NEW_MESSAGE_TEXT = "CURRENT_NEW_MESSAGE_TEXT";
 
-const dialogsReducer = (state, action) => {
+const initialState = {
+  dialogsData: [
+    {
+      id: 1,
+      name: "qw",
+      messagesData: [
+        { id: 1, message: "Lorem ipsum dolor sit." },
+        {
+          id: 2,
+          message: "Lorem sit amet, consectetur adipisicing elit. Accusanti,",
+        },
+        { id: 3, message: "Lorem ipsum dolor sit amet." },
+      ],
+    },
+    {
+      id: 2,
+      name: "we",
+      messagesData: [
+        {
+          id: 1,
+          message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "er",
+      messagesData: [
+        {
+          id: 1,
+          message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        },
+        { id: 2, message: "Lorem ipsum dolor sit amet, consectetur." },
+      ],
+    },
+    {
+      id: 4,
+      name: "rt",
+      messagesData: [
+        {
+          id: 1,
+          message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        },
+        { id: 2, message: "Lorem ipsum dolor sit amet, consectetur." },
+        { id: 3, message: "Lorem ipsum dolor sit." },
+      ],
+    },
+    {
+      id: 5,
+      name: "ty",
+      messagesData: [
+        { id: 1, message: "Lorem ipsum dolor sit amet, consectetur." },
+        { id: 2, message: "Lorem ipsum dolor sit." },
+      ],
+    },
+  ],
+  tempMessageData: [{ message: "" }],
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SEND_MESSAGE:
       _addMessageFunc(state, action.dialogId);
