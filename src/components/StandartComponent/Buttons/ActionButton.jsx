4e4e611e31e3cd5ff829b7cdc035058f4122b0d5
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-11-09 23:03:28 +0300

    add style for standart button

--- src/components/StandartComponent/Buttons/ActionButton.jsx
+++ src/components/StandartComponent/Buttons/ActionButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./ButtonSend.module.css";
 
 const ActionButton = (props) => {
   return (
-    <div>
+    <div className={styles.button}>
       <button onClick={props.buttonAction} name={props.buttonName}>
         {props.buttonName}
       </button>
