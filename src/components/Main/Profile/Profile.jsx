49b796d8d61eef8dae06d73e550e1ed70afdb188
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-11-14 16:47:20 +0300

    L59: change profile

--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -3,24 +3,47 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import Posts from "./Posts/Posts";
 import { connect } from "react-redux";
 import defaultProfileAvatar from "../../../default/images/defaultProfileAvatar.png";
+import axios from "axios";
+import { setUserProfileAC } from "../../../redux/profileReducer";
 
-let mapDispatchToProps = (store) => {
+let mapStateToProps = (store) => {
   return {
     userProfile: store.profilePage.profileData,
+    defaultId: store.defaultId,
     defaultProfileAvatar,
   };
 };
 
-const ProfileInfoContainer = connect(mapDispatchToProps, {})(ProfileInfo);
-
-const Profile = () => {
-  return (
-    <div>
-      <div>Profile</div>
-      <ProfileInfoContainer />
-      <Posts />
-    </div>
-  );
+let mapDispatchToProps = (dispatch) => {
+  return {
+    setUserProfile: (userProfile) => {
+      dispatch(setUserProfileAC(userProfile));
+    },
+  };
 };
+
+class ProfileContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/profile/${this.props.defaultId}`
+      )
+      .then((response) => {
+        this.props.setUserProfile(response.data);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Profile</div>
+        <ProfileInfo {...this.props} />
+        <Posts />
+      </div>
+    );
+  }
+}
+
+const Profile = connect(mapStateToProps, mapDispatchToProps)(ProfileContainer);
 
 export default Profile;
