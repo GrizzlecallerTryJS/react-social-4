bffbf388b04a5beb76d4190febbf28c2977417aa
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-11-23 23:42:09 +0300

    L66_h: add thunk getAuth

---
+++ src/redux/authFunc/authThunkCreators.js
@@ -0,0 +1,12 @@
+import { AuthAPI } from "../../api/api";
+import { setAuthAC } from "../authReducer";
+
+export const getAuthTC = () => {
+  return (dispatch) => {
+    AuthAPI.getAuth().then((data) => {
+      if (data.resultCode === 0) {
+        dispatch(setAuthAC(data.data));
+      }
+    });
+  };
+};
