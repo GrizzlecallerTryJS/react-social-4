18633fc447b6d126a6481b4bd216690ddaaf69ec
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-11-27 15:11:07 +0300

    L74: add componentDidUpdate

--- src/components/Main/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Main/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class ProfileStatus extends React.Component {
   state = {
     editMode: false,
-    status: this.props.profileStatus,
+    profileStatus: this.props.profileStatus,
   };
 
   setEditMode = (mode) => {
@@ -11,22 +11,34 @@ class ProfileStatus extends React.Component {
       editMode: mode,
     });
     if (!mode) {
-      this.props.setUserStatus(this.props.userId, this.state.status);
+      this.props.setUserStatus(this.props.userId, this.state.profileStatus);
     }
   };
 
   onChangeAction = (e) => {
     this.setState({
-      status: e.currentTarget.value,
+      profileStatus: e.currentTarget.value,
     });
   };
+
+  componentDidUpdate(
+    prevProps: Readonly<P>,
+    prevState: Readonly<S>,
+    snapshot: SS
+  ) {
+    if (prevProps.profileStatus !== this.props.profileStatus) {
+      this.setState({
+        profileStatus: this.props.profileStatus,
+      });
+    }
+  }
 
   render() {
     if (this.state.editMode) {
       return (
         <span>
           <input
-            value={this.state.status}
+            value={this.state.profileStatus}
             onChange={this.onChangeAction}
             onBlur={() => {
               this.setEditMode(false);
