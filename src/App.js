b16a8a22d0f34b6055843b3a8535dddb5e1cdc83
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2021-12-26 14:02:35 +0300

    L80: move isAuth check from header to app

--- src/App.js
+++ src/App.js
@@ -5,27 +5,51 @@ import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
+import { connect } from "react-redux";
+import { getAuthTC } from "./redux/authFunc/authThunkCreators";
 
-const App = () => {
-  return (
-    <div className="app_wrapper">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-        <Main />
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-    </div>
-  );
+let mapStateToProps = (state) => {
+  return {
+    isAuth: state.authState.isAuth,
+  };
 };
 
-export default App;
+let mapDispatchToProps = (dispatch) => {
+  return {
+    getAuth: () => {
+      dispatch(getAuthTC());
+    },
+  };
+};
+
+class App extends React.Component<{}> {
+  componentDidMount() {
+    if (!this.props.isAuth) {
+      this.props.getAuth();
+    }
+  }
+
+  render() {
+    return (
+      <div className="app_wrapper">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Main />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
